1e3da3606fe87d0ef8f5d2e9d343d4be1ec44608
Anita Bui-Martinez <adb2221@columbia.edu>
2023-04-24 17:16:29 -0400

    Makefile and Parts 0 & 1 :)

--- src/chatclient.c
+++ src/chatclient.c
@@ -15,12 +15,39 @@ char outbuf[MAX_MSG_LEN + 1];
 
 int handle_stdin() {
     /* TODO */
+	return 0; 
 }
 
 int handle_client_socket() {
     /* TODO */
+	return 0; 
 }
 
+// no signal handling in the client 
 int main(int argc, char **argv) {
     /* TODO */
+	// check for right number of command line args 
+	if(argc == 1 || argc > 3){
+		fprintf(stderr, "Usage: %s <server IP> <port>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	
+	// check for valid IP address using inet_pton()
+	struct in_addr inaddr; 
+	int rval; 
+	if((rval = inet_pton(AF_INET, argv[1], &inaddr)) == 0){
+		fprintf(stderr, "IPv4 address %s is invalid.\n", argv[1]);
+	       return EXIT_FAILURE;
+	}	       
+	else if(rval == -1){
+		perror("inet_pton"); 
+		return EXIT_FAILURE; 
+	}
+
+	// chack that port number is in range 
+	int port = atoi(argv[2]);
+	if(port < 1024 || port > 65535){
+		fprintf(stderr, "Error: Port %s is out of range. Port must be in range [1024, 65535].\n", argv[2]); 
+		return EXIT_FAILURE; 
+	}
 }
